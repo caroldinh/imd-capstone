7a803cd34b87dcfbe95a6e6406934ff3dd348a96
caroldinh <ctdinh443@gmail.com>
2024-05-08 22:53:58 -0400

    add gary scene

---
+++ fq-webgl/gillman_scene/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-FairyQuestWebGL-0.1";
+const contentToCache = [
+    "Build/gillman_scene.loader.js",
+    "Build/gillman_scene.framework.js",
+    "Build/gillman_scene.data",
+    "Build/gillman_scene.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
